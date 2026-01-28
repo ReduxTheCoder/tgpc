f3b0dbe64ddf2fb1b35f82333e39270cb4f01005
ReduxTheCoder <gennarobiondi@gmail.com>
2026-01-28 16:45:21 +0100

    refactor(file_utils.c): use concat_str instead
    before file_utils.c used snprintf to concat strings, but now that
    there's src/utils.c, and it adds the function concat_str() it uses that

--- src/file_utils.c
+++ src/file_utils.c
@@ -5,6 +5,7 @@
 # include <string.h>
 # include <sys/stat.h>
 # include "../include/struct.h"
+# include "../include/utils.h"
 
 typedef char * path;
 
@@ -16,22 +17,49 @@ char * build_project_path(const char * project_name) {
         return NULL;
     }
 
-    // (+ 2 is for the null terminators)
-    size_t len = strlen(cwd) + strlen(project_name) + 2;
-    
-    char * result = malloc(len);
-    
-    if (!result) {
-        perror("malloc");
-        return NULL;
+    return concat_str(cwd, project_name, 0);
+}
+
+static char * get_template_by_lang(const ProgrammingLanguage lang) {
+    switch (lang) {
+        case PYTHON:
+            return "print(\"Hello, World!\")";
+            break;
+        case C:
+            return "# include <stdio.h>\n\nint main(const int argc, const char ** argv) {\n\tprintf(\"Hello, World\");\n\treturn 0;\n}";
+            break;
+        case CPP:
+            return "# include <iostream>\n\nint main(const int argc, const char ** argv) {\n\tstd::cout << \"Hello, World!\\n\";\n\treturn 0;\n}";
+            break;
+        case RUST:
+            return "fn main() {\n\tprintln!(\"Hello, World!\")\n}";
+            break;
+        case JAVA:
+            return "public class HelloWorld {\\n    public static void main(String[] args) {\\n        System.out.println(\\\"Hello, World!\\\");\\n    }\\n}";
+            break;
+        default:
+            printf("Warning: Language not recognized, this will make an empty template file");
+            return "";
+            break;
     }
-    
-    snprintf(result, len, "%s/%s", cwd, project_name);
-    return result;
 }
 
 void create_project(const ProgramConfig * config) {
     path project_parent = build_project_path(config->ProjectName);
     mkdir(project_parent, 0777);
+
+    chdir(project_parent);
     free(project_parent);
+
+    FILE * metadata = fopen(".tgpc_meta", "a");
+    fprintf(metadata, "%s\n", config->ProgrammingLanguageString);
+    fclose(metadata);
+
+    char * main_name = concat_str("main.", config->ProgrammingLanguageString, 1);
+    FILE * main = fopen(main_name, "a");
+    free(main_name);
+
+    char * template_str = get_template_by_lang(config->ProjectLang);
+
+    fprintf(main, "%s\n", template_str);
 }
