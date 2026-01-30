119f1f99a2225ec913c0fc1aec9a4afff92279d9
ReduxTheCoder <gennarobiondi@gmail.com>
2026-01-30 19:15:37 +0100

    feat(): add license to the top of all source files

--- include/logging.h
+++ include/logging.h
@@ -1,3 +1,24 @@
+
+/*
+ * TGPC - Command-line project scaffolder
+ * Copyright (c) 2026 ReduxTheCoder
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * File: <logging.h>
+ * Description: <pretty printing>
+*/
 # ifndef LOGGING_H
 # define LOGGING_H
 
