e38dd92e1ac3a27f2b18ee52f5824676a72da806
ReduxTheCoder <gennarobiondi@gmail.com>
2026-02-13 17:38:05 +0100

    Setup run command

---
+++ include/commands/run.hpp
@@ -0,0 +1,6 @@
+#ifndef RUN_HPP
+#define RUN_HPP
+
+void run_project(void);
+
+#endif
---
+++ src/command/run.cpp
@@ -0,0 +1,8 @@
+#include "commands/run.hpp"
+#include "structs/metadata.hpp"
+
+void run_project(void) {
+    std::string programming_language = Metadata::get(Metadata::Key::PROGRAMMING_LANGUAGE);
+
+
+}
